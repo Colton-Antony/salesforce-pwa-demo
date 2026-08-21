1bdd362d0ee4471e43cfa97c2faa6987f80c3b0a
Antony Colton <antony@thecoltons.co.uk>
2026-08-21 15:26:07 +0100

    Theme colour ASDA

---
+++ overrides/app/theme/foundations/colors.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+const colors = {
+    transparent: 'transparent',
+    current: 'currentColor',
+    black: '#000000',
+    white: '#FFFFFF',
+
+    // Replace or add your primary brand palette with Asda Green
+    brand: {
+        50: '#F0F9E8',
+        100: '#D5F0BE',
+        200: '#BAE694',
+        300: '#9EDC6A',
+        400: '#83D240',
+        500: '#78BE21', // Main Asda Green
+        600: '#609B1A',
+        700: '#487413',
+        800: '#304E0D',
+        900: '#182706'
+    },
+
+    gray: {
+        50: '#F3F3F3',
+        100: '#E5E5E5',
+        200: '#C9C9C9',
+        300: '#AEAEAE',
+        400: '#A0A0A0',
+        500: '#939393',
+        600: '#747474',
+        700: '#5C5C5C',
+        800: '#444444',
+        900: '#181818'
+    },
+    // ... keep the rest of your color scales below ...
+}
+
+export default colors
